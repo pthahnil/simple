4859872dda499d6cf232b0f8d50cd2ef85bd205d
lixiaorong <lixiaorong@wimift.com>
2021-01-22 13:49:48 +0800

    反序列化更改

--- simple-common/src/main/java/com/simple/xrcraft/common/utils/bean/deserializer/StringDeserializer.java
+++ simple-common/src/main/java/com/simple/xrcraft/common/utils/bean/deserializer/StringDeserializer.java
@@ -13,10 +13,10 @@ import java.io.IOException;
  * @author pthahnil
  * @date 22018/6/20 16:51
  */
-public class MapStringDeserializer extends JsonDeserializer<String> {
+public class StringDeserializer extends JsonDeserializer<String> {
 
 	@Override
-	public String deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException,JsonProcessingException {
+	public String deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
 		JsonNode node = jp.getCodec().readTree(jp);
 		String retVal = null;
 		if(node.isTextual()) {
